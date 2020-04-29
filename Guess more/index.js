c48e93dc693c53e1dcc786d2c77a0be6b8035497
Mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-04-29 16:07:59 +0530

    Fourteencommit

---
+++ Guess more/index.js
@@ -0,0 +1,25 @@
+//Snippet
+const x = 9;
+ 
+function f1(val){
+    val = val + 1;
+    return val;
+}
+    //when the function f() fired it will increase verbal x
+    // so the output the value will be 10
+f1(x);
+
+// the function will not change the variable x value it will be still the sam so the output is 9
+console.log(x);
+const y = { x: 9};
+
+function f2(val){
+    val.x = val.x + 1;
+    return val;
+
+}
+// when the function f2() fired it will increase the object y
+// so the output the value will be 10
+f2(y);
+// the function here will change the value of the object the output  is 10
+console.log(y);
